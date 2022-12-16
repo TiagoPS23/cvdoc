b0d75c59bfcb205efd388d1e938d2b8db80c2f3e
Tiago <tiago.sobreira@hotmail.com>
2022-12-16 16:21:47 +0000

    second commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Testing site',
+  tagline: 'Hope this works',
   url: 'https://github.com',
   baseUrl: '/TiagoPS23/cvdoc/',
   onBrokenLinks: 'throw',
@@ -16,8 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TiagoPS23', // Usually your GitHub org/user name.
+  projectName: 'cvdoc', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -57,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'My site',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
---
+++ src/pages/index.md
@@ -0,0 +1,53 @@
+# Tiago Pedrosa Sobreira
+
+## EXPERIÊNCIA PROFISSIONAL
+
+### José Monteiro de Sousa LDA
+
+**Assistente de Loja-Comércio de peças automóveis**
+
+-> Abril 2016 até Junho 2021
+- Atendimento ao público
+- Gestão e acompanhamento de mercadoria
+- Gestão e organização de stock
+- Elaboração de orçamentos
+
+
+### PCTESTE
+**Estagiário de Informática**
+
+-> Abril 2015 até Junho 2015
+- Reparação de dispositivos eletrónicos
+- Atendimento ao público
+
+
+## FORMAÇÃO ACADÉMICA
+
+### Instituto Politécnico de Leiria
+
+Licenciatura em Marketing | Setembro 2021 - o momento 
+
+
+## DESPORTO
+
+### Futebol - Sport Clube Leiria e Marrazes 
+- 2005 - o momento 
+
+### Treinador Futebol Infantil 
+- 2018/2019 - Sport Clube Leiria e Marrazes
+
+### Outros Desportos 
+- Trail Running, Ciclismo e Padel nos tempos livres
+
+
+## IDIOMAS
+
+- Português Nativo 
+- Inglês B2
+
+
+
+
+
+
+
